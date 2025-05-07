f65f2a90e6f69ba2929d905f605b0fc0f580fd35
thaoHT <thaothuhoang24@gmail.com>
2025-05-07 22:13:22 +0700

    add headers Authorization cho API

--- app/src/main/java/com/example/travelapp/Interface/ApiService.java
+++ app/src/main/java/com/example/travelapp/Interface/ApiService.java
@@ -8,14 +8,25 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("/api/item")
-    Call<List<ItemDomain>> searchLocation(@Query("q") String query);
+    @Headers({
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ii1PUGRjWnoxNXVYR0RCNVByLUpzIiwiZW1haWwiOiJodXkwNDA0MjRAZ21haWwuY29tIiwiZnVsbG5hbWUiOiJIdXkgTmd1eeG7hW4iLCJpYXQiOjE3NDY2Mjk5NDMsImV4cCI6MTc1NTI2OTk0M30.8k2JhasGwr9M5En-4957Hj98AEMSs1OpKjZn4qSdfc0"
+    })
+    Call<List<ItemDomain>> searchLocation( @Query("q") String query);
     @GET("/api/item")
+    @Headers({
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ii1PUGRjWnoxNXVYR0RCNVByLUpzIiwiZW1haWwiOiJodXkwNDA0MjRAZ21haWwuY29tIiwiZnVsbG5hbWUiOiJIdXkgTmd1eeG7hW4iLCJpYXQiOjE3NDY2Mjk5NDMsImV4cCI6MTc1NTI2OTk0M30.8k2JhasGwr9M5En-4957Hj98AEMSs1OpKjZn4qSdfc0"
+    })
     Call<List<ItemDomain>> GetItem(@Query("c") int categoryId);
     @GET("api/order")
+    @Headers({
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ii1PUGRjWnoxNXVYR0RCNVByLUpzIiwiZW1haWwiOiJodXkwNDA0MjRAZ21haWwuY29tIiwiZnVsbG5hbWUiOiJIdXkgTmd1eeG7hW4iLCJpYXQiOjE3NDY2Mjk5NDMsImV4cCI6MTc1NTI2OTk0M30.8k2JhasGwr9M5En-4957Hj98AEMSs1OpKjZn4qSdfc0"
+    })
     Call<List<MyTicket>> getOrders(@Query("u") String username);
 }
 
